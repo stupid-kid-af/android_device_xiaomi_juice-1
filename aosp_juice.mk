f99c65733e46a9aa921906a2bf17020617e8a545
Ashutosh Tripathi <pushpatripathi1111@gmail.com>
2022-01-23 22:45:51 +0530

    Update aosp_juice.mk

--- aosp_juice.mk
+++ aosp_juice.mk
@@ -7,7 +7,7 @@
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
 # Inherit some common Pixel stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_juice
